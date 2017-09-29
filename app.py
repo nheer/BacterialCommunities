8316e19e9bd59e237808adf64dfa52d25d6d81e7
nheer <nheer@mit.edu>
2017-09-29 15:54:44 -0400

    Update datasets to be consistent

--- app.py
+++ app.py
@@ -9,54 +9,106 @@ import pandas as pd
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
 
-df = pd.read_csv('com_20_all_all.csv')
-df2 = pd.read_csv('com_20_all_all_para.csv')
+df_loc_com = pd.read_csv('bac_com_20_df.csv')
+df_com_geoclim = pd.read_csv('bac_com_20_geo.csv')
 with open('bac_20_top5taxa.pkl', 'rb') as f:
     taxa = pickle.load(f)
 
 
 model_explanation='''
 ## Identifying Bacterial Communities
-This project is a proof of concept using machine learning to identify bacterial communities for market segmentation in the probiotic space.
+This project is a proof of concept using machine learning to identify bacterial
+communities for market segmentation in the probiotic space.
 
 ### Bacteria are everywhere, and we are only just learning how to harness them
-There are more bacterial cells in a human body than human cells. Bacteria shape the world around them. They act like contract workers for larger organisms, performing chemistry that animals and plants are unable to do for themselves. Sometimes they are needed break down molecules, other times to defend against other bacteria that is harmful. Until recently, most bacterial species were a mystery to biologists. Only bacteria that could be grown in a lab could be studied, or even identified, and this is only possible for a very small subset of all bacterial species. With recent advances in DNA sequencing technology, we can now record DNA from all the bacteria present in a given sample. Even with this powerful tool, sometimes all we know about a sample is that it contains DNA not present in other bacteria previously observed.
+There are more bacterial cells in a human body than human cells. Bacteria shape
+the world around them. They act like contract workers for larger organisms,
+performing chemistry that animals and plants are unable to do for themselves.
+Sometimes they are needed break down molecules, other times to defend against
+other bacteria that is harmful. Until recently, most bacterial species were a
+mystery to biologists. Only bacteria that could be grown in a lab could be studied,
+or even identified, and this is only possible for a very small subset of all bacterial
+species. With recent advances in DNA sequencing technology, we can now record
+DNA from all the bacteria present in a given sample. Even with this powerful tool,
+sometimes all we know about a sample is that it contains DNA not present in other
+bacteria previously observed.
 
 ### What are probiotics?
-Probiotics apply the notion that some bacteria are beneficial for human, animal or plant health and package those benefits as a commercial product.
+Probiotics apply the notion that some bacteria are beneficial for human, animal
+or plant health and package those benefits as a commercial product.
 
 ### Bacterial subpopulations can be targeted with probiotics
-Current probiotics products use a one-size-fits-all approach. But there is increasing information about how bacteria interact with each other, giving us the opportunity to target different probiotics to people and places with different bacterial populations. This is especially true for plant probiotics, where the beneficial bacteria need to be optimized not only for the plants themselves, but also for the bacteria that live in the surrounding soil. Practical market segmentation is particularly important in this field because the number of possible bacteria species stretches into the millions. Market segments would ideally be communities of positively interacting bacteria. We can assume that if bacteria have positive interactions they are more likely to occur together.
+Current probiotics products use a one-size-fits-all approach. But there is
+increasing information about how bacteria interact with each other, giving us
+the opportunity to target different probiotics to people and places with different
+bacterial populations. This is especially true for plant probiotics, where the
+beneficial bacteria need to be optimized not only for the plants themselves, but
+also for the bacteria that live in the surrounding soil. Practical market
+segmentation is particularly important in this field because the number of
+possible bacteria species stretches into the millions. Market segments would
+ideally be communities of positively interacting bacteria. We can assume that if
+bacteria have positive interactions they are more likely to occur together.
 
 ### We have data on the presence of bateria at locations across the US
-Data for this analysis is from dust samples collected from across the US as part of the "Wild life of our homes" citizen science project and published [here](/pnas.org/content/112/18/5756.full). Participants from over 1000 locations collected dust samples, and over 130,000 bacterial species (OTUs) were identified. As with most data of this type, the data is only 3% dense, meaning that most species are not observed at most locations. This is important for determining the appropriate clustering mechanism.
+Data for this analysis is from dust samples collected from across the US as part
+of the "Wild life of our homes" citizen science project and published
+[here](/pnas.org/content/112/18/5756.full). Participants from over 1000 locations
+collected dust samples, and over 130,000 bacterial species (OTUs) were identified.
+As with most data of this type, the data is only 3% dense, meaning that most
+species are not observed at most locations. This is important for determining
+the appropriate clustering mechanism.
 
 ### Latent Dirichlet allocation can identify bacterial subpopulations
-For this project, I chose to use latent Dirichlet allocation (LDA) to define bacterial communities. We assume that when each location is sampled (e.g., a doorway is swabbed) we are picking up bacteria from multiple coexisting bacterial communities. Additionally, multiple bacterial communities might contain the same species. The LDA model fits with these assumptions. In brief, LDA is a generative probabilistic model in which each sampled location is a random mixture over the latent communities defined by the model and each community is defined by a distribution over bacterial species.
+For this project, I chose to use latent Dirichlet allocation (LDA) to define
+bacterial communities. We assume that when each location is sampled (e.g., a
+doorway is swabbed) we are picking up bacteria from multiple coexisting bacterial
+communities. Additionally, multiple bacterial communities might contain the same
+species. The LDA model fits with these assumptions. In brief, LDA is a generative
+probabilistic model in which each sampled location is a random mixture over the
+latent communities defined by the model and each community is defined by a
+distribution over bacterial species.
 
 ### We can identify 20 bacterial communities across the US
-The classic way to validate an LDA model is by evaluating the perplexity of the model. The perplexity is a measure of the likelihood of producing the observations in a test dataset assuming that the distributions defined by the model are true. Specifically, it can be calculated as the inverse geometric mean of the per-species likelihood. The smaller the perplexity, the more accurately the model predicts the observed data. I used the perplexity score to define the minimal number of communities required to describe the bacterial diversity in my dataset; the minimum is approximately 20 communities. (See slide show for graph).
+The classic way to validate an LDA model is by evaluating the perplexity of the
+model. The perplexity is a measure of the likelihood of producing the
+observations in a test dataset assuming that the distributions defined by the
+model are true. Specifically, it can be calculated as the inverse geometric mean
+of the per-species likelihood. The smaller the perplexity, the more accurately
+the model predicts the observed data. I used the perplexity score to define the
+minimal number of communities required to describe the bacterial diversity in the
+dataset; the minimum is approximately 20 communities. (See slide show for graph).
 
 ### The bacterial communities are easily visualized
 #### Taxa
-In addition to calculating the perplexity on a held-out test dataset, I examined the bacterial species present in each community. I found that some communities were defined completely by unknown bacterial species, while in other communities, the top weighted bacteria were all in the same phylum, class, or order. Explore more in the Taxa tab.
+In addition to calculating the perplexity on a held-out test dataset, I examined
+the bacterial species present in each community. I found that some communities
+were defined completely by unknown bacterial species, while in other communities,
+the top weighted bacteria were all in the same phylum, class, or order. Explore
+more in the Taxa tab.
 
 #### Locations
-Using a separate dataset containing latitude and longitude data for each location, I visualized the distribution of each community across the US. Explore in the Locations tab.
+Using a separate dataset containing latitude and longitude data for each location,
+I visualized the distribution of each community across the US. Explore in the
+Locations tab.
 
 #### Climate data
-I also determined the mutual information as well as a weighted mean and standard deviation metric for the elevation, mean annual precipitation and mean annual temperature associated with each location. Interestingly, none of the communities are strongly defined by these location parameters alone. Explore further in the Climate tab.
+I also determined the mutual information as well as a weighted mean and standard
+deviation metric for the elevation, mean annual precipitation and mean annual
+temperature associated with each location. Interestingly, none of the communities
+are strongly defined by these location parameters alone. Explore further in the
+Climate tab.
 '''
 
 def plot_parameter(characteristic, ptitle):
+    '''Plot community weighted mean and std of elevation, precipitation and temperature'''
     return {
         'data': [
             go.Scatter(
                 x=list(range(1,21)),
-                y=df2[characteristic+'mean'],
+                y=df_com_geoclim[characteristic+' Mean'],
                 error_y={
                     'type': 'data',
-                    'array': df2[characteristic+'std'],
+                    'array': df_com_geoclim[characteristic+' Std'],
                     'visible': True
                 },
                 mode='markers',
@@ -79,6 +131,7 @@ def plot_parameter(characteristic, ptitle):
 
 app.config.supress_callback_exceptions = True
 
+#: Set up navbar
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
@@ -87,9 +140,9 @@ app.layout = html.Div([
                 dcc.Link('Bacterial States of America', href='/', className='navbar-brand'),
             ], className='navbar-header'),
             html.Ul([
-                html.Li([dcc.Link('Project Explanation', href='/model')]),
+                html.Li([dcc.Link('Project Explanation', href='/explanation')]),
                 html.Li([dcc.Link('Location Explorer', href='/')]),
-                html.Li([dcc.Link('Community Properties', href='/about')]),
+                html.Li([dcc.Link('Climate Properties', href='/geoclimate')]),
                 html.Li([dcc.Link('Taxa Explorer', href='/taxa')]),
                 html.Li([html.A('Slides', href='https://docs.google.com/presentation/d/e/2PACX-1vTzvJQWZRATdPWl7HDEMyJuMKlDC0N7FMkiUxn31qLSaTGd3Vope0FdFP94cJns13K95nlNNXJPxwJn/pub?start=false&loop=false&delayms=3000')])
             ], className='nav navbar-nav')
@@ -98,6 +151,7 @@ app.layout = html.Div([
     html.Div(id='page-content', style={'padding-top': '80px'})
 ])
 
+#: Layout of Location explorer tab
 index_layout = html.Div([
     html.Div([
         html.Div([
@@ -125,7 +179,8 @@ index_layout = html.Div([
     ], className='row'),
 ], className='container-fluid')
 
-about_layout = html.Div([
+#layout of Climate propertires tab
+geoclimate_layout = html.Div([
     html.Div([
         html.Div([
             html.Div([
@@ -169,6 +224,7 @@ about_layout = html.Div([
     ], className='row')
 ], className='container-fluid')
 
+#Layout taxa explorer page
 taxa_layout = html.Div([
     html.Div([
         html.Div([
@@ -195,7 +251,8 @@ taxa_layout = html.Div([
     ], className='row'),
 ], className='container-fluid')
 
-model_layout = html.Div([
+#: Layout project explanation page.
+explanation_layout = html.Div([
     html.Div([
         html.Div([
             dcc.Markdown(children=model_explanation)
@@ -226,18 +283,18 @@ def generate_table(community):
     dash.dependencies.Output('location-values', 'figure'),
     [dash.dependencies.Input('map', 'hoverData')])
 def location_values(hoverData):
-    s = df[df['ID'] == hoverData['points'][0]['customdata']]
+    '''Make linear range plots of elevation, temperature, precipitation'''
+    s = df_loc_com[df_loc_com['ID'] == hoverData['points'][0]['customdata']]
     scales = ['<b>Precipitation</b>', '<b>Temperature</b>', '<b>Elevation</b>']
-    max_P = df['MeanAnnualPrecipitation'].max().round(-1) + 10
-    max_T = df['MeanAnnual Temperature'].max().round(-1) + 10
-    max_E = df['Elevation'].max().round(-2) + 100
+    max_P = df_loc_com['MeanAnnualPrecipitation'].max().round(-1) + 10
+    max_T = df_loc_com['MeanAnnual Temperature'].max().round(-1) + 10
+    max_E = df_loc_com['Elevation'].max().round(-2) + 100
     max_values = [max_P, max_T, max_E]
 
-    # Add Scale Titles to the Plot
     traces = []
     for i in range(len(scales)):
         traces.append(go.Scatter(
-            x=[-.25], # Pad the title - a longer scale title would need a higher value
+            x=[-.25],
             y=[0],
             text=scales[i],
             mode='text',
@@ -246,8 +303,7 @@ def location_values(hoverData):
             xaxis='x'+str(i+1),
             yaxis='y'+str(i+1)
         ))
-    # Create Scales
-    ## Since we have 7 lables, the scale will range from 0-6
+
     shapes = []
     for i in range(len(scales)):
         shapes.append({'type': 'line',
@@ -255,16 +311,14 @@ def location_values(hoverData):
                        'y0': 0, 'y1': 0,
                        'xref':'x'+str(i+1), 'yref':'y'+str(i+1)})
 
-    y_domains = [[0, .33], [.33, .67], [.67, 1]] # Split for 3 scales
+    y_domains = [[0, .33], [.33, .67], [.67, 1]]
 
-    # Define Y-Axes
     yaxes = []
     for i in range(len(scales)):
         yaxes.append({'domain': y_domains[i], 'range':[-.5,1],
                       'showgrid': False, 'showline': False,
                       'zeroline': False, 'showticklabels': False})
 
-    # Define X-Axes (and set scale labels)
     xaxes = []
     for i in range(len(scales)):
         xaxes.append({'anchor':'y'+str(i+1), 'range':[-0.5,1.1],
@@ -284,7 +338,6 @@ def location_values(hoverData):
                 text=loc_values[i], hoverinfo='text', showlegend=False
         ))
 
-    # Put all elements of the layout together
     layout = {'shapes': shapes,
               'title': 'Climate data in {}, {}'.format(
                 s.iloc[0]['City'].title().strip(),
@@ -305,16 +358,17 @@ def location_values(hoverData):
     dash.dependencies.Output('map', 'figure'),
     [dash.dependencies.Input('community-slider', 'value')])
 def update_map(community):
+    '''Map community values that update with slider'''
     return {
         'data': [{
-            'lat': df['Latitude'],
-            'lon': df['Longitude'],
+            'lat': df_loc_com['Latitude'],
+            'lon': df_loc_com['Longitude'],
             'marker': {
                 'color': 'white',
-                'size': 40 * df['Community {}'.format(community)],
+                'size': 40 * df_loc_com['Community {}'.format(community)],
                 'opacity': 0.6
             },
-            'customdata': df['ID'],
+            'customdata': df_loc_com['ID'],
             'type': 'scattermapbox'
         }],
         'layout': {
@@ -332,7 +386,8 @@ def update_map(community):
     dash.dependencies.Output('communities-dist', 'figure'),
     [dash.dependencies.Input('map', 'hoverData')])
 def update_plot(hoverData):
-    s = df[df['ID'] == hoverData['points'][0]['customdata']]
+    '''Bar graph of community weights for selected location'''
+    s = df_loc_com[df_loc_com['ID'] == hoverData['points'][0]['customdata']]
     return {
         'data': [go.Bar(
             x=['Com. {}'.format(com+1) for com in range(20)],
@@ -365,10 +420,10 @@ def update_plot(hoverData):
     dash.dependencies.Output('page-content', 'children'),
     [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/model':
-        return model_layout
-    if pathname == '/about':
-        return about_layout
+    if pathname == '/explanation':
+        return explanation_layout
+    if pathname == '/geoclimate':
+        return geoclimate_layout
     if pathname == '/taxa':
         return taxa_layout
     else:
